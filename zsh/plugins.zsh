c79510ebb0bc9620199f502c695235b14133ecaa
chen <c252156290@gmail.com>
2020-12-10 11:04:49 +0800

    update

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -25,8 +25,8 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 
 # antigen theme robbyrussell
 # antigen theme ys
-antigen theme dracula/zsh
-THEME=dracula
+# antigen theme dracula/zsh
+THEME=dracula/zsh
 # THEME=denysdovhan/spaceship-prompt
 antigen list | grep $THEME; if [ $? -ne 0 ]; then antigen theme $THEME; fi
 antigen apply
